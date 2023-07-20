5357a7714c2b56bf12b3c7bfe73239ba4ed04d4b
Malgorzata Polit-Zagun <m.polit.zagun@gmail.com>
2023-07-20 22:02:12 +0200

    Corrected bug in main.js file. Added other js file for notes page

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
 const firstName = "Małgosia";
-console.log(`Hej, jestem ${firstName} i witam Cię na mojej stronie :)`)
-
-let wtfPlayground3
-wtfPlayground3=document.querySelector('.playground__wtf--js').innerHTML;
-console.log(wtfPlayground3);
-document.querySelector('.playground__wtf--js').innerHTML='JAVA SCRIPT'
-wtfPlayground3=document.querySelector('.playground__wtf--js').innerHTML;
-console.log(wtfPlayground3);
-console.log('Zmienione przez JS!')
+console.log(`Hej, jestem ${firstName} i witam Cię na mojej stronie :)`);
