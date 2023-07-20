d642bf36d69dddce36c337d1560deccb90d4b6ae
Malgorzata Polit-Zagun <m.polit.zagun@gmail.com>
2023-07-20 21:55:37 +0200

    Added js script changing part of text

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const firstName = "Małgosia";
 console.log(`Hej, jestem ${firstName} i witam Cię na mojej stronie :)`)
+
+let wtfPlayground3
+wtfPlayground3=document.querySelector('.playground__wtf--js').innerHTML;
+console.log(wtfPlayground3);
+document.querySelector('.playground__wtf--js').innerHTML='JAVA SCRIPT'
+wtfPlayground3=document.querySelector('.playground__wtf--js').innerHTML;
+console.log(wtfPlayground3);
+console.log('Zmienione przez JS!')
