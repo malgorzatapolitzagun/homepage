819388a5421dab43e0815ac9f0c88dba9c33426a
Malgorzata Polit-Zagun <m.polit.zagun@gmail.com>
2023-07-24 21:40:03 +0200

    Fix in main-notes.js

--- main-notes.js
+++ main-notes.js
@@ -2,9 +2,9 @@ const firstName = "Małgosia";
 console.log(`Hej, jestem ${firstName} i witam Cię na mojej stronie :)`)
 
 let wtfPlayground3
-wtfPlayground3=document.querySelector('.playground__wtf--js').innerHTML;
+wtfPlayground3 = document.querySelector('.playground__wtf--js').innerHTML;
 console.log(wtfPlayground3);
-document.querySelector('.playground__wtf--js').innerHTML='JAVA SCRIPT'
-wtfPlayground3=document.querySelector('.playground__wtf--js').innerHTML;
+document.querySelector('.playground__wtf--js').innerHTML = 'JAVA SCRIPT'
+wtfPlayground3 = document.querySelector('.playground__wtf--js').innerHTML;
 console.log(wtfPlayground3);
-console.log('Zmienione przez JS!')
+console.log('Zmienione przez JS!');
